3357286a7bc5868c3e04325737384a7c1bb2da0d
Janu <januanggana07@gmail.com>
2022-11-07 10:18:54 +0700

    janu

---
+++ soal ka roy.py
@@ -0,0 +1,25 @@
+#soal1: Hello World to Python
+print('Hello World to Python')
+
+#Soal2: Aritmatika di Python
+print(3+4)
+print(12-7)
+print(3*2)
+print(14//2)
+
+#Soal3: Assign Variable dan Tipe Data Integer, Float
+a = 14
+b = 2
+print(a//b)
+print(a/b)
+
+#Soal4: Tipe Data
+usia = "16 Tahun" 
+kata = "usiaku:"
+print ("{} {}".format(kata,usia))
+
+
+
+
+
+
